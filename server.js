35db89ef361be737e6172c40ffd381c1a3ddafc8
frnado <fernandogutilla@hotmail.com>
2025-05-15 19:25:25 -0300

    CORS para local

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-import express from 'express'
-import database from './database.js'
-import 'dotenv/config'
-import router from './routes/user.js'
-import routerTask from './routes/task.js'
-import routerTasks from './routes/allTasks.js'
-const app = express()
-const PORT = process.env.PORT || 3131
-import cors from 'cors'
+import express from "express";
+import database from "./database.js";
+import "dotenv/config";
+import router from "./routes/user.js";
+import routerTask from "./routes/task.js";
+import routerTasks from "./routes/allTasks.js";
+const app = express();
+const PORT = process.env.PORT || 3131;
+import cors from "cors";
 
-app.use(express.json())
+app.use(express.json());
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); 
+      const allowedOrigins = [
+        "http://localhost:5173/", 
+        "https://seufrontend.vercel.app", 
+      ];
 
-    const allowed = origin.endsWith('.vercel.app') || origin === 'https://vercel.app';
+     const allowed = allowedOrigins.includes(origin) || origin.endsWith('.vercel.app');
 
-    if (allowed) {
-      callback(null, true);
-    } else {
-      callback(new Error('Não permitido por CORS'));
-    }
-  },
-  credentials: true
-}));
-app.use(router)
-app.use(routerTask)
-app.use(routerTasks)
+      if (allowed) {
+        callback(null, true);
+      } else {
+        callback(new Error("Não permitido por CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+app.use(router);
+app.use(routerTask);
+app.use(routerTasks);
 
 const start = async () => {
   try {
-      await database.sync();
-      console.log('Banco syncado');
-      app.listen(PORT, () => console.log(`Subiu a pipa do vovô. port:${PORT}`));
-
+    await database.sync();
+    console.log("Banco syncado");
+    app.listen(PORT, () => console.log(`Subiu a pipa do vovô. port:${PORT}`));
   } catch (error) {
-      console.error("Erro ao conectar ao banco:", error);
+    console.error("Erro ao conectar ao banco:", error);
   }
-}
+};
 
-start()
+start();
